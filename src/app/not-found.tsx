ddb64649ff68c39d793b3bdd703488129ce56133
squ1dggly <fluffyspiderstudios@gmail.com>
2025-05-17 15:43:32 -1000

    added suspense

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div className="flex w-full flex-col items-center justify-center gap-12 px-6">
+            <h1 className="text-5xl">404 Page Not Found</h1>
+        </div>
+    );
+}
